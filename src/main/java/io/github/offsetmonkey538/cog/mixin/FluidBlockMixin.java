8e0343dcc963f23ec5819633625a614ac56fb2b8
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-05 00:11:52 +0300

    Rename ExampleMixin and make it mixin into the FluidBlock class.

--- src/main/java/io/github/offsetmonkey538/cog/mixin/FluidBlockMixin.java
+++ src/main/java/io/github/offsetmonkey538/cog/mixin/FluidBlockMixin.java
@@ -3,14 +3,13 @@ package io.github.offsetmonkey538.cog.mixin;
 import io.github.offsetmonkey538.cog.CogMain;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.block.FluidBlock;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
+@Mixin(FluidBlock.class)
+public class FluidBlockMixin {
 
 	}
 }
