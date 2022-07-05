ed294bb23a23e3c49263ba984e46a0074e717b1d
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-05 18:05:32 +0300

    Use `Tag` instead of `TagKey` because 1.18.1 and below doesn't have `TagKey`.

--- src/main/java/io/github/offsetmonkey538/cog/mixin/FluidBlockMixin.java
+++ src/main/java/io/github/offsetmonkey538/cog/mixin/FluidBlockMixin.java
@@ -1,40 +1,29 @@
 package io.github.offsetmonkey538.cog.mixin;
 
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FluidBlock;
-import net.minecraft.tag.TagKey;
+import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntry;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.Random;
 
 @Mixin(FluidBlock.class)
 public class FluidBlockMixin {
 
-	private static final TagKey<Block> ORES = TagKey.of(Registry.BLOCK_KEY, new Identifier("c", "ores"));
+	private static final Tag<Block> ORES = TagFactory.BLOCK.create(new Identifier("c", "ores"));
 
 	private static final Random rng = new Random();
 
 	@Redirect(method = "receiveNeighborFluids", at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;COBBLESTONE:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC))
 	private Block cog$receiveNeighborFluids() {
-		if (rng.nextInt(3) > 0)
-			return Blocks.COBBLESTONE;
-		return Registry.BLOCK.getEntryList(ORES).flatMap(t -> {
-
-			List<RegistryEntry<Block>> list = t.stream().toList();
-
-			if (list.isEmpty())
-				return Optional.empty();
-			return Optional.of(list.get(rng.nextInt(list.size())));
-
-		}).orElse(RegistryEntry.of(Blocks.CAKE)).value();
+		if (!ORES.values().isEmpty() && rng.nextInt(3) == 0)
+			return ORES.getRandom(rng);
+		return Blocks.COBBLESTONE;
 	}
 }
