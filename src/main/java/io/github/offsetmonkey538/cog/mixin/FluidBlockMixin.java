f28c434f81c75dbc1acced9eaa59b4f34386caf7
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-05 15:01:24 +0300

    Load the `c:ores` common tag. Create a random number generator.

--- src/main/java/io/github/offsetmonkey538/cog/mixin/FluidBlockMixin.java
+++ src/main/java/io/github/offsetmonkey538/cog/mixin/FluidBlockMixin.java
@@ -2,14 +2,22 @@ package io.github.offsetmonkey538.cog.mixin;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidBlock;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.registry.Registry;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.block.FluidBlock;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(FluidBlock.class)
 public class FluidBlockMixin {
+
+	private static final TagKey<Block> ORES = TagKey.of(Registry.BLOCK_KEY, new Identifier("c", "ores"));
+
+	private static final Random rng = Random.create();
 
 	@Redirect(method = "receiveNeighborFluids", at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;COBBLESTONE:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC))
 	private Block cog$receiveNeighborFluids() {
